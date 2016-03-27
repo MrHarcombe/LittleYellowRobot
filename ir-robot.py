519ea5aa4702d6809ba684136e662abb755609b5
Ian Harcombe <ian.harcombe@gmail.com>
2016-03-27 12:13:08 +0100

    *mutter* Brain failure - too much chocolate, perhaps?

--- ir-robot.py
+++ ir-robot.py
@@ -28,7 +28,7 @@ robot_power = (
 
 evdev_device = 0
 if (len(sys.argv) > 0):
-  if is_digit(sys.argv[0]):
+  if sys.argv[0].is_digit():
     evdev_device = int(sys.argv[0])
 
 print("Initialised, now handling events")
